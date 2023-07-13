5c5759551c3fd2b48e2cd82d207d0538650dc088
bnkala <nkala.bryan3@gmail.com>
2023-07-13 14:35:01 +0200

    edit form tutorial

--- django_project1/blog/views.py
+++ django_project1/blog/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render, redirect
+from django.contrib import messages
 from .models import Post 
 from .forms import PostForm
 #from django.http import HttpResponse
@@ -12,8 +13,10 @@ def create_post(request):
         form = PostForm(request.POST)
         if form.is_valid():
             form.save()
+            messages.success(request, 'This post has been created successfully')
             return redirect('posts')
         else: 
+            messages.error(request, 'Please correct the following orders')
             return render(request, 'blog/post_form.html', {'form':form})
 
 def home(request):
